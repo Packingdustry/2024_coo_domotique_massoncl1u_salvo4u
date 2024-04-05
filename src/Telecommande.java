4802695c72726af7ac26f6c4027be0076e00975e
Packingdustry <alois.mc@hotmail.com>
2024-04-05 15:27:04 +0200

    ajoute différentes méthodes dans Telecomande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class Telecommande {
+    private List<Lampe> lampes;
+
+    public Telecommande() {
+        lampes = new ArrayList<>();
+    }
+
+    public void ajouterLampe(Lampe lampe) {
+        lampes.add(lampe);
+    }
+
+    public void allumerLampe(int indiceLampe) {
+        lampes.get(indiceLampe).allumer();
+    }
+
+    public void eteindreLampe(int indiceLampe) {
+        lampes.get(indiceLampe).eteindre();
+    }
+
+    public void activerTout() {
+        for (Lampe lampe : lampes) {
+            lampe.allumer();
+        }
+    }
 }
